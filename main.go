697da15a1a4ca4017e280eddb7f047f6e58fca2d
Pavel Konovalov <pasha@nettlecom.com>
2026-06-15 22:27:43 +0300

    Added model generator for Go port

--- main.go
+++ main.go
@@ -47,6 +47,8 @@ func main() {
 		runGenCode(os.Args[2:])
 	case "viewmodel":
 		runViewModel(os.Args[2:])
+	case "gengolibmodel":
+		runGenGoLibModel(os.Args[2:])
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command: %s\n", cmd)
 		printUsage()
@@ -56,14 +58,16 @@ func main() {
 
 func printUsage() {
 	fmt.Println("Usage:")
-	fmt.Println("  genmodel  <ICD file> [-ied <name>] [-ap <name>] [-out <output>] [-modelprefix <prefix>] [-initializeonce]")
-	fmt.Println("  genconfig <ICD file> [-ied <name>] [-ap <name>] [<output file>]")
-	fmt.Println("  gencode   <ICD file> [<output file>]")
-	fmt.Println("  viewmodel <ICD file> [-ied <name>] [-ap <name>] [-t] [-u] [-s] [-a] [<output file>]")
+	fmt.Println("  genmodel      <ICD file> [-ied <name>] [-ap <name>] [-out <output>] [-modelprefix <prefix>] [-initializeonce]")
+	fmt.Println("  genconfig     <ICD file> [-ied <name>] [-ap <name>] [<output file>]")
+	fmt.Println("  gencode       <ICD file> [<output file>]")
+	fmt.Println("  viewmodel     <ICD file> [-ied <name>] [-ap <name>] [-t] [-u] [-s] [-a] [<output file>]")
 	fmt.Println("    -t  print type list")
 	fmt.Println("    -u  print unused types")
 	fmt.Println("    -s  print model structure")
 	fmt.Println("    -a  print data attribute list")
+	fmt.Println("  gengolibmodel <ICD file> [-ied <name>] [-ap <name>] [-pkg <package>] [<output file>]")
+	fmt.Println("    Generates a Go source file with BuildModel() for github.com/PVKonovalov/libiec61850-Go")
 }
 
 func runGenModel(args []string) {
@@ -238,6 +242,58 @@ func runViewModel(args []string) {
 
 	err = tools.RunModelViewer(stream, output, iedName, accessPointName,
 		printTypeList, printUnusedTypes, printModelStructure, printDataAttributes)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func runGenGoLibModel(args []string) {
+	if len(args) < 1 {
+		fmt.Println("Usage: gengolibmodel <ICD file> [-ied <name>] [-ap <name>] [-pkg <package>] [<output file>]")
+		os.Exit(1)
+	}
+
+	icdFile := args[0]
+	iedName := ""
+	accessPointName := ""
+	packageName := "model"
+	output := os.Stdout
+
+	for i := 1; i < len(args); i++ {
+		switch args[i] {
+		case "-ied":
+			i++
+			iedName = args[i]
+		case "-ap":
+			i++
+			accessPointName = args[i]
+		case "-pkg":
+			i++
+			packageName = args[i]
+		default:
+			if !startsWithDash(args[i]) {
+				f, err := os.Create(args[i])
+				if err != nil {
+					fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
+					os.Exit(1)
+				}
+				defer f.Close()
+				output = f
+			} else {
+				fmt.Fprintf(os.Stderr, "Unknown option: %q\n", args[i])
+			}
+		}
+	}
+
+	stream, err := os.Open(icdFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: cannot open %s: %v\n", icdFile, err)
+		os.Exit(1)
+	}
+	defer stream.Close()
+
+	err = tools.RunGoModelGenerator(stream, output, packageName, iedName, accessPointName)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "ERROR: %v\n", err)
 		os.Exit(1)
